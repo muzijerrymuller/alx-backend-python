904770a2eecd7407ce535bcf6e097d00ec2eb06c
muzijerrymuller <jerrymuller883@gmail.com>
2024-10-21 13:00:27 +0000

    function that does the floor insqr meters

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+"""
+this function is a a annoatted variable types function
+and will convert a float to a string
+"""
+
+
+def to_str(n: float) -> str:
+    '''Convertsion of a floating-point number to a string.
+    '''
+    return str(n)
