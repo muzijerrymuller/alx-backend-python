919b75fab2f82bf3068f328d8fd9113a0aa0f741
muzijerrymuller <jerrymuller883@gmail.com>
2024-10-21 13:11:03 +0000

    function that does the floor insqr meters

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Converts a floating-point number to its string representation.
+
+This function takes a floating-point number as an argument and
+converts it to a string format. The conversion is achieved using
+the built-in `str()` function, which returns the string version
+of the given float. This can be particularly useful for
+formatting output or for use in string concatenation.
+"""
+
+
+a: int = 1                     # an integer with a value of 1
+pi: float = 3.14               # a float with a value of 3.14
+i_understand_annotations: bool = True  # a boolean with a value of True
+school: str = "Holberton"      # a string with a value of "Holberton"
