21209786396c486cd1c1f6973bb320edf6196f96
muzijerrymuller <jerrymuller883@gmail.com>
2024-10-21 11:52:59 +0000

    function that does the floor insqr meters

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import math
+
+# Function to return the floor of the given float number
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the float number n.
+    
+    Args:
+    n (float): The float number to get the floor value of.
+    
+    Returns:
+    int: The largest integer less than or equal to n.
+    """
+    return math.floor(n)
