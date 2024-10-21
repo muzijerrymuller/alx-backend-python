d51b5ee0572d198b42250e4bbfe714d6fcf45a9f
muzijerrymuller <jerrymuller883@gmail.com>
2024-10-21 09:28:32 +0000

    adding using annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-this is an addition function that uses annotation
+this is an addition function
+that uses annotation
 """
-
 def add(a: float, b: float) -> float:
     return a + b
