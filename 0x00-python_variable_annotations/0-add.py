8a8febc607266a06db0f58ea59597f78e9ad3bb3
muzijerrymuller <jerrymuller883@gmail.com>
2024-10-21 09:35:33 +0000

    with the use of annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 """
-this is an addition function
-that uses annotation
+This module provides a function for adding two floats.
 """
+
 def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
     return a + b
