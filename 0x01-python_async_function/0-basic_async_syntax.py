2232facd49747e5b5da48d0ba37b8ebe98c0c4f5
muzijerrymuller <jerrymuller883@gmail.com>
2024-10-26 12:06:28 +0000

    correction

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-import asyncio
-import random
-
-
 """
 This module provides a utility coroutine for introducing a randomized delay in an asynchronous program.
 
 It contains the following function:
 - wait_random: Asynchronously waits for a random delay between 0 and a specified maximum delay (default is 10 seconds).
 """
+import asyncio
+import random
+
+
 async def wait_random(max_delay: int = 10) -> float:
     """
     Waits for a random delay between 0 and max_delay seconds and returns the delay.
