dfd5c586868ea6e236c35d78d3918bcfd382ac79
muzijerrymuller <jerrymuller883@gmail.com>
2024-11-04 09:00:49 +0000

    test utils

--- 0x03-Unittests_and_integration_tests/test.utils.py
+++ 0x03-Unittests_and_integration_tests/test.utils.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """
-This module contains unit tests for the `access_nested_map` function from
-the `utils` module. The tests verify that the function correctly retrieves
-values from a nested dictionary using a specified path of keys.
+Test for access_nested_map function
 """
 import unittest
 import requests
@@ -14,25 +12,82 @@ from parameterized import parameterized
 
 class TestAccessNestedMap(unittest.TestCase):
     """
-    Unit test case for testing the `access_nested_map` function.
-    This test case class uses the `parameterized.expand` decorator to
-    run multiple test cases on the `access_nested_map` function,
-    verifying that it returns the expected values for given nested maps and paths.
+    Tests the access_nested_map function
     """
-
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Sequence, expected: int) -> None:
         """
-        Test `access_nested_map` with various nested maps and paths.
+        Test the access_nested_map method.
         Args:
-            nested_map (dict): The nested dictionary to retrieve values from.
-            path (tuple): The sequence of keys to follow in the dictionary.
-            expected: The expected value at the end of the path.
-        Asserts:
-            The value retrieved by `access_nested_map` matches `expected`.
+            nested_map (Dict): A dictionary that may have nested dictionaries
+            path (List, tuple, set): Keys to get to the required value in the
+                                     nested dictionary
         """
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+        response = access_nested_map(nested_map, path)
+        self.assertEqual(response, expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                                         path: Sequence) -> None:
+        """
+        Test the access_nested_map method raises an error when expected to
+        Args:
+            nested_map (Dict): A dictionary that may have nested dictionaries
+            path (List, tuple, set): Keys to get to the required value in the
+                                     nested dictionary
+        """
+        with self.assertRaises(Exception):
+            access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Test the get_json function
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch("requests.get")
+    def test_get_json(self, test_url, test_payload, mock_requests_get):
+        """
+        Test the get_json method to ensure it returns the expected output.
+        Args:
+            url: url to send http request to
+            payload: expected json response
+        """
+        mock_requests_get.return_value.json.return_value = test_payload
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
+        mock_requests_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test the memoization decorator, memoize
+    """
+    def test_memoize(self):
+        """
+        Test that utils.memoize decorator works as intended
+        """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, 'a_method') as mock_object:
+            test = TestClass()
+            test.a_property()
+            test.a_property()
+            mock_object.assert_called_once()
