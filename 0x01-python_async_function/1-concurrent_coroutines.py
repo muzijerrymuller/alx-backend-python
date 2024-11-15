703c87fd1a4715a9455c7fa60203f42f34ecaa8f
muzijerrymuller <jerrymuller883@gmail.com>
2024-11-15 11:02:26 +0000

    asynchronous coding

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-This module defines an async function `wait_n`, which runs `wait_random`
+This module defines an async function `wait_n`,
+which runs `wait_random`
 a specified number of times with a maximum delay.
 """
 
@@ -11,20 +12,13 @@ from 0-basic_async_syntax.py import wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Runs `wait_random` n times with a given max delay and returns the delays in ascending order.
-    
+    Runs `wait_random` n times with a given max delay and
+    returns the delays in ascending order.
     Args:
         n (int): Number of times to run `wait_random`.
         max_delay (int): Maximum delay value for each call.
-    
     Returns:
         List[float]: List of delays in ascending order.
     """
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    delays = []
-    
-    for task in asyncio.as_completed(tasks):
-        delay = await task
-        delays.append(delay)
-    
-    return delays
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]i
+    return [await task for task in asyncio.as_completed(tasks)]
